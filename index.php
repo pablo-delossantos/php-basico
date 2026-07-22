0b62b412db601d08ea402c25305e73c9dfce2c4e
Pablo de los Santos <estudiodedalo@gmail.com>
2026-07-22 01:04:30 +0000

    modificacion index.php clase 10

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php
 
-echo "Hola, esto está hecho con php";
+// echo "Hola mundo \n";
+
+$nombre = "Pablo";
+$apellido = "de los Santos";
+
+// echo "Yo me llamo " . $nombre . " " . $apellido . "\n";
+
+// echo "Yo me llamo $nombre $apellido \n";
+
+// echo "El resultado de 4 x 5 es " . (4 * 5) . "\n";
+
+$personas = [
+    "Carlos" => 22,
+    "Pablo" => 47,
+    "Juan" => 65
+];
+
+// var_dump( $personas );
+// print_r( $personas );
+
+// Eso es un comentario
+# Esto es otro comentario
+
+/*
+Esto es
+un comentario
+multilínea
+*/
+
+echo "\n";
